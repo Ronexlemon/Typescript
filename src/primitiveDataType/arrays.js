df668d1990482c6a7d76b8fb57f5bd973b08c02a
RonexLemon <ronexondimu@gmail.com>
2024-02-03 12:29:02 +0300

    array<string|number>

--- src/primitiveDataType/arrays.js
+++ src/primitiveDataType/arrays.js
@@ -2,10 +2,12 @@
 //normal declaration
 let fruits = ["melon", "yogo", "orange", "mango"];
 let fruits1 = ["melon1", "yogo1", "orange1", "mango1"];
+let fruitsPriceArray = ["hello", 1, "yollow", 45];
 function getArrayAndReturnAnArray(array) {
     return array;
 }
 const CallFunctions = () => {
     console.log("Array returned", getArrayAndReturnAnArray(fruits));
+    console.log("Array returned", getArrayAndReturnAnArray(fruitsPriceArray));
 };
 CallFunctions();
--- src/primitiveDataType/arrays.ts
+++ src/primitiveDataType/arrays.ts
@@ -4,13 +4,16 @@ let fruits: string[] =["melon","yogo","orange","mango"]
 
 let fruits1: Array<string> = ["melon1","yogo1","orange1","mango1"]
 
+let fruitsPriceArray:Array<string|number> = ["hello",1,"yollow",45]
 
-function getArrayAndReturnAnArray(array:Array<string>):Array<string>{
+
+function getArrayAndReturnAnArray(array:Array<string|number>):Array<string|number>{
     return array;
 
 }
 
 const CallFunctions =()=>{
     console.log("Array returned",getArrayAndReturnAnArray(fruits))
+    console.log("Array returned",getArrayAndReturnAnArray(fruitsPriceArray))
 }
 CallFunctions()
