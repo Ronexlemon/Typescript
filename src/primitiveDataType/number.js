7e9d5722691029602c8876a5b01d72acd92df95a
RonexLemon <ronexondimu@gmail.com>
2024-02-03 12:06:01 +0300

    tofix & exponential

--- src/primitiveDataType/number.js
+++ src/primitiveDataType/number.js
@@ -7,3 +7,18 @@ console.log(first); // 123
 console.log(second); // 14287
 console.log(third); // 255
 console.log(fourth); // 57 
+function returnsPersonDetails(peronaDetails) {
+    return `The person Details are:  age: ${peronaDetails.age} , salary: ${peronaDetails.salary}, years: ${peronaDetails.years}`;
+}
+function getNumberExponential({ num, expo }) {
+    return num.toExponential(expo);
+}
+function getToFixOfANumber({ number, tofix }) {
+    return number.toFixed(tofix);
+}
+const call = () => {
+    console.log(returnsPersonDetails({ age: 10, salary: 3000, years: 2023 }));
+    console.log(getNumberExponential({ num: 5, expo: 5 }));
+    console.log(getToFixOfANumber({ number: 10.948696849, tofix: 4 }));
+};
+call();
--- src/primitiveDataType/number.ts
+++ src/primitiveDataType/number.ts
@@ -7,3 +7,33 @@ console.log(first);  // 123
 console.log(second); // 14287
 console.log(third);  // 255
 console.log(fourth); // 57 
+
+type arrayOfNumber ={
+    age:number,
+    salary:number
+    years:number
+
+}
+
+
+function returnsPersonDetails(peronaDetails:arrayOfNumber){
+    return `The person Details are:  age: ${peronaDetails.age} , salary: ${peronaDetails.salary}, years: ${peronaDetails.years}`
+
+}
+
+function getNumberExponential({ num, expo }: { num: number, expo?: number }): string {
+    return num.toExponential(expo);
+}
+
+function getToFixOfANumber({number,tofix}:{number:number,tofix?:number}):string{
+    return number.toFixed(tofix)
+}
+
+
+const call =()=>{
+    console.log(returnsPersonDetails({age:10,salary:3000,years:2023}))
+    console.log(getNumberExponential({num:5,expo:5}))
+    console.log(getToFixOfANumber({number:10.948696849,tofix:4}))
+
+}
+call():
