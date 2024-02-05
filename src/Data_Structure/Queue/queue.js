c9819ddae720dbacf8a9b9099315d3a66392dd4f
RonexLemon <ronexondimu@gmail.com>
2024-02-05 05:46:11 +0300

    queue

--- src/Data_Structure/Queue/queue.js
+++ src/Data_Structure/Queue/queue.js
@@ -5,4 +5,47 @@ class Queue {
         this.size = 0;
         this.items = [];
     }
+    //enqueue
+    enqueue(value) {
+        this.items.push(value);
+        this.size++;
+    }
+    //dequeue
+    dequeue() {
+        if (this.isEmpty()) {
+            throw new Error("Dequeue operation on an empty queue");
+        }
+        this.items.shift();
+        this.size--;
+    }
+    //search
+    contains(value) {
+        if (this.isEmpty()) {
+            throw new Error("Queue is empty");
+        }
+        return this.items.includes(value);
+    }
+    //access first element
+    peek() {
+        return this.isEmpty() ? undefined : this.items[0];
+    }
+    //check empty
+    isEmpty() {
+        return this.size === 0;
+    }
+    //getsize
+    getSize() {
+        return this.size;
+    }
 }
+const queue = new Queue();
+console.log("The queue is Empty:", queue.isEmpty());
+console.log("The Size of the queue is before enqueue:", queue.getSize());
+queue.enqueue(1);
+queue.enqueue(2);
+console.log("Front of the queue:", queue.peek());
+queue.enqueue(3);
+console.log("The Size of the queue is after enqueue:", queue.getSize());
+queue.dequeue();
+console.log("The Size of the queue is after denqueue:", queue.getSize());
+console.log("Front of the queue:", queue.peek());
--- src/Data_Structure/Queue/queue.ts
+++ src/Data_Structure/Queue/queue.ts
@@ -7,7 +7,53 @@ class Queue<T>{
         this.items =[];
     }
     //enqueue
+    enqueue(value:T):void{
+        this.items.push(value)
+        this.size ++;
+    }
     //dequeue
+    dequeue():void{
+        if(this.isEmpty()){
+            throw new  Error("Dequeue operation on an empty queue")
+
+        }
+        this.items.shift()
+        this.size --;
+    }
     //search
-    //access
+    contains(value:T):boolean{
+        if(this.isEmpty()){
+            throw new Error("Queue is empty")
+        }
+       return  this.items.includes(value);
+    }
+    
+    //access first element
+    peek():T |undefined{
+        return this.isEmpty()? undefined: this.items[0];
+    }
+
+    //check empty
+    isEmpty():boolean{
+        return this.size ===0;
+    }
+    //getsize
+    getSize():number{
+        return this.size;
+    }
 }
+
+const queue = new Queue<number>()
+
+console.log("The queue is Empty:",queue.isEmpty())
+console.log("The Size of the queue is before enqueue:",queue.getSize())
+
+queue.enqueue(1)
+queue.enqueue(2)
+console.log("Front of the queue:",queue.peek())
+queue.enqueue(3)
+
+console.log("The Size of the queue is after enqueue:",queue.getSize())
+queue.dequeue()
+console.log("The Size of the queue is after denqueue:",queue.getSize())
+console.log("Front of the queue:",queue.peek())
