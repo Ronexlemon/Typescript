c540f7597a515672c2c179c4c5a31b6166135325
RonexLemon <ronexondimu@gmail.com>
2024-02-08 05:31:27 +0300

    extends

--- src/primitiveDataType/interface.js
+++ src/primitiveDataType/interface.js
@@ -2,4 +2,12 @@
 function printPersonDetails(p) {
     console.log(`Hello ${p.name} ,you have turned ${p.age} years`);
 }
+function getPersonDetailsAndCalculateNetPay(pd) {
+    const net = calCulateNetPay({ salary: pd.salary, tax: pd.tax });
+    console.log(`Welcome ${pd.name} your gross salary is ${pd.salary} , tax ${pd.tax} and netPay ${net}`);
+}
+function calCulateNetPay(np) {
+    return (np.salary - np.tax);
+}
 printPersonDetails({ age: 18, name: "John Doe" });
+getPersonDetailsAndCalculateNetPay({ name: "hamas", age: 20, salary: 100000, tax: 5000, rate: 10 });
--- src/primitiveDataType/interface.ts
+++ src/primitiveDataType/interface.ts
@@ -4,8 +4,30 @@ interface Person{
 
 }
 
+//interface is extended
+interface MoreDetails extends Person{
+    salary:number;
+    rate : number;
+    tax:number;
+
+}
+type netPay={
+    salary:number;
+    tax:number;
+}
 function printPersonDetails(p:Person){
     console.log(`Hello ${p.name} ,you have turned ${p.age} years`);
 }
 
+function getPersonDetailsAndCalculateNetPay(pd:MoreDetails){
+    const net = calCulateNetPay({salary:pd.salary,tax:pd.tax});
+    console.log(`Welcome ${pd.name} your gross salary is ${pd.salary} , tax ${pd.tax} and netPay ${net}`);
+
+}
+
+function calCulateNetPay(np:netPay):number{
+    return (np.salary - np.tax);
+}
+
 printPersonDetails({age:18,name:"John Doe"})
+getPersonDetailsAndCalculateNetPay({name:"hamas",age:20,salary:100000,tax:5000,rate:10})
