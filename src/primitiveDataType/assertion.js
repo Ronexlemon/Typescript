ce9348e40417803563e28e5e4a3d90014100a169
RonexLemon <ronexondimu@gmail.com>
2024-02-25 13:21:37 +0300

    type assertion

---
+++ src/primitiveDataType/assertion.js
@@ -0,0 +1,6 @@
+"use strict";
+let a = 0xa;
+let myCode = a;
+console.log(typeof (myCode));
+console.log(typeof (a));
+console.log(a);
---
+++ src/primitiveDataType/assertion.ts
@@ -0,0 +1,8 @@
+let a:any =0xa;
+
+let myCode = <number> a;
+
+console.log(typeof(myCode))
+
+console.log(typeof(a))
+console.log(a)
