e7d8847895fad3d03cbfad99e96934167ba8b913
RonexLemon <ronexondimu@gmail.com>
2024-02-25 13:55:24 +0300

    return:assertion

--- src/primitiveDataType/assertion.js
+++ src/primitiveDataType/assertion.js
@@ -4,10 +4,19 @@ empl.name = "ronex";
 empl.salary = 1000;
 var empl2 = [];
 empl2.push({ name: "john doe", salary: 1000000 });
+///using as keyword
+var two = 20202;
+var newCode = two;
+//
+const sum = (sal, nam) => {
+    return { name: nam, salary: sal };
+};
 let a = 0xa;
 let myCode = a;
 console.log(typeof (myCode));
 console.log(typeof (a));
+console.log(typeof (newCode));
 console.log(a);
 console.log(empl);
 console.log(empl2);
+console.log(sum(100, "lemon"));
--- src/primitiveDataType/assertion.ts
+++ src/primitiveDataType/assertion.ts
@@ -13,15 +13,19 @@ var empl = <EmployeeTwo>{}
 empl.name ="ronex"
 empl.salary= 1000
 var empl2: Array<EmployeeOne> =[]
+
 empl2.push({name:"john doe",salary:1000000});
 
 
+///using as keyword
 
+var two:any = 20202;
+var newCode = two as number;
 
-
-
-
-
+//
+const sum =(sal:number,nam:string):EmployeeOne=>{
+    return {name:nam,salary:sal}
+}
 
 
 
@@ -33,8 +37,11 @@ let myCode = <number> a;
 console.log(typeof(myCode))
 
 console.log(typeof(a))
+console.log(typeof(newCode))
 console.log(a)
 
 
 console.log(empl)
 console.log(empl2)
+console.log(sum(100,"lemon"))
+
