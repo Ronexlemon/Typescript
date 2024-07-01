d6be950ad0302167565fcaa01385709e27b5084b
RonexLemon <ronexondimu@gmail.com>
2024-07-01 13:05:24 +0300

    protected mod

--- src/Class/AccessModifier.js
+++ src/Class/AccessModifier.js
@@ -1,4 +1,19 @@
 //Private, public, protected
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
     if (kind === "m") throw new TypeError("Private method is not writable");
     if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
@@ -23,5 +38,36 @@ var Modifier = /** @class */ (function () {
     return Modifier;
 }());
 _Modifier_name = new WeakMap();
+var Private = /** @class */ (function () {
+    function Private(_name) {
+        this.name = _name;
+    }
+    Private.prototype.readName = function () {
+        console.log("the name is", this.name);
+    };
+    return Private;
+}());
+var Person = /** @class */ (function () {
+    function Person(_name) {
+        this.namee = _name;
+    }
+    return Person;
+}());
+var Emplo = /** @class */ (function (_super) {
+    __extends(Emplo, _super);
+    function Emplo(_name, _department) {
+        var _this = _super.call(this, _name) || this;
+        _this.department = _department;
+        return _this;
+    }
+    Emplo.prototype.getDetails = function () {
+        console.log("the name is", this.namee, "and i work on this department", this.department);
+    };
+    return Emplo;
+}(Person));
 var mod = new Modifier("John doe");
+var pri = new Private("Doen");
+var emp = new Emplo("John doe", "IT");
+emp.getDetails();
+pri.readName();
 mod.readName();
--- src/Class/AccessModifier.ts
+++ src/Class/AccessModifier.ts
@@ -24,8 +24,29 @@ class Private{
             }
 }
 
+class Person{
+    //can be accessed inside the class and outside the class -> through inheritance
+    protected namee:string;
+    constructor(_name:string){
+        this.namee = _name}
+    
+}
+class Emplo extends Person{
+    private department:string
+    constructor(_name:string, _department:string){
+        super(_name)
+        this.department = _department}
+
+public getDetails(){
+    console.log("the name is", this.namee, "and i work on this department",this.department)
+}
+
+}
+
 let mod = new Modifier("John doe")
 let pri = new Private("Doen")
+let emp = new Emplo("John doe", "IT")
+emp.getDetails()
 pri.readName()
 
 mod.readName()
