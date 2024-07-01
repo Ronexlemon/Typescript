2a6214f37ccaef9caefead56c0467dbd7a870b9d
RonexLemon <ronexondimu@gmail.com>
2024-07-01 12:59:44 +0300

    private mod

--- src/Class/AccessModifier.ts
+++ src/Class/AccessModifier.ts
@@ -14,6 +14,18 @@ class Modifier{
 
 }
 
+class Private{
+    //can only be accessed inside the classonly and not outside
+    private name:string
+    constructor(_name:string){
+        this.name = _name}
+        readName(){
+            console.log("the name is", this.name)
+            }
+}
+
 let mod = new Modifier("John doe")
+let pri = new Private("Doen")
+pri.readName()
 
 mod.readName()
